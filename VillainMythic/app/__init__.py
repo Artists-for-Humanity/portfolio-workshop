81b618c617634a1c2458d72294b4a3f820c4a3d4
VillainMythic <87036074+VillainMythic@users.noreply.github.com>
2021-11-03 16:40:50 -0400

    .flask setup

--- VillainMythic/app/__init__.py
+++ VillainMythic/app/__init__.py
@@ -10,13 +10,13 @@ def create_app(test_config=None):
         __name__,
         instance_relative_config=True,
         static_url_path="",
-        static_folder="static",
+        static_folder="./static",
     )
 
     # a simple page that says hello
     @app.route("/")
     def homepage():
         items = [{"title": "Item 1"}, {"title": "Item 2"}]
-        return render_template("index.html", items=items)
+        return render_template("home.html", items=items)
 
     return app
