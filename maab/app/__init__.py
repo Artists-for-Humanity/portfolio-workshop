580864f026fa9fc75728eca0d79c9b85dfbf21be
Maab <maabmarei8@gmail.com>
2021-11-04 17:31:55 -0400

    work

--- maab/app/__init__.py
+++ maab/app/__init__.py
@@ -21,5 +21,41 @@ def create_app(test_config=None):
             "image": "images/girl.jpeg"
         }]
         return render_template('home.html', items=items)
+    # a simple page that says hello
+
+    @app.route('/explore')
+    def explorepage():
+        images = [{
+            "src": "images/ocean.jpg",
+            "alt": "ocean img"
+        }, {
+            "src": "images/tan.jpg",
+            "alt": "tan img"
+        }, {
+            "src": "images/city.jpg",
+            "alt": "city img"
+        }, {
+            "src": "images/human.jpg",
+            "alt": "human img"
+        }, {
+            "src": "images/portrait.jpg",
+            "alt": "portrait img"
+        }, {
+            "src": "images/purple.jpg",
+            "alt": "purple img"
+        }, {
+            "src": "images/supplies.jpg",
+            "alt": "supplies img"
+        }, {
+            "src": "images/ribbon.jpg",
+            "alt": "ribbon img"
+        }, {
+            "src": "images/black.jpg",
+            "alt": "black img"
+        }, {
+            "src": "images/drums.jpg",
+            "alt": "drums img"
+        }]
+        return render_template('explore.html', images=images)
 
     return app
