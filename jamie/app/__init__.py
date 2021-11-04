07f2cae0a9db2476a0c91ae9af308c1cd21316b8
Jamie D <itisjamied@gmail.com>
2021-11-04 16:56:31 -0400

    added new pages into flask init file

--- jamie/app/__init__.py
+++ jamie/app/__init__.py
@@ -5,6 +5,16 @@ from flask import (
     render_template
 )
 
+userInfo = {
+    "stephen": {
+        "age": 29,
+        "address": "sdfdsf"
+    },
+    "jamie": {
+        "age": 18
+    }
+}
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True, static_url_path='', static_folder='./static')
@@ -27,6 +37,47 @@ def create_app(test_config=None):
             "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
         },]
         return render_template('home.html', items=items)
+
+    @app.route('/gallery')
+    def galleryPage():
+        items = [{
+            "title": "Item 1",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        }, {
+            "title": "Item 2",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },
+        {
+            "title": "Item 6",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },{
+            "title": "Item 6",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },]
+        return render_template('gallery.html', items=items)
+
+    @app.route('/commission')
+    def commisionPage():
+        items = [{
+            "title": "Item 1",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        }, {
+            "title": "Item 2",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },
+        {
+            "title": "Item 6",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },{
+            "title": "Item 6",
+            "image": "Mai, Shu Ru. Still Life Hunger Games Pear Green Table.jpg"
+        },]
+        return render_template('commission.html', items=items)
+
+    @app.route('/profile/<username>/edit')
+    def namePage(username):
+        user = userInfo[username]
+        return render_template('name.html', user=user)
     
     return app
 
