7481b3788fbad98642493606981f911c5ad0966d
Jonathan <Jwu28@bostonk12.org>
2021-11-11 17:26:55 -0500

    added a view all page

--- jonathan/app/__init__.py
+++ jonathan/app/__init__.py
@@ -4,6 +4,25 @@ from flask import Flask
 from flask import (
     render_template
 )
+people  = [{
+    "name": "Manuel D. Kirts",
+    "image":"/images/Manuel D. Kirts.jpeg"
+},{
+    "name": "William C. Yu",
+    "image": "/images/William C. Yu.jpeg"
+}, {
+    "name": "Amie C. Gram",
+    "image": "/images/Amie C. Gram .jpeg"
+}, {
+    "name": "Chris Peanuts",
+    "image": "/images/Leonard B. Andrews .jpeg"
+},{
+    "name": "Orville M. Bober",
+    "image": "/images/Orville M. Bober .jpeg"
+},{
+    "name":"Jose H. Schaffer",
+    "image":"/images/Jose H. Schaffer .jpeg"
+}]
 
 def create_app(test_config=None):
     # create and configure the app
@@ -12,26 +31,16 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def homepage():
-        items  = [{
-            "name": "Manuel D. Kirts",
-            "image":"/images/Manuel D. Kirts.jpeg"
-        },{
-            "name": "William C. Yu",
-            "image": "/images/William C. Yu.jpeg"
-        }, {
-            "name": "Amie C. Gram",
-            "image": "/images/Amie C. Gram .jpeg"
-        }, {
-            "name": "Chris Peanuts",
-            "image": "/images/Leonard B. Andrews .jpeg"
-        },{
-            "name": "Orville M. Bober",
-            "image": "/images/Orville M. Bober .jpeg"
-        },{
-            "name":"Jose H. Schaffer",
-            "image":"/images/Jose H. Schaffer .jpeg"
-        }]
-        return render_template('home.html', items=items)
+        return render_template('home.html', people=people[:3])
+
+    @app.route('/people')
+    def peoplepage():
+        return render_template('people.html', people=people)
+
+    @app.route('/people/<id>')
+    def personpage(id):
+        person = people[int(id)]
+        return render_template('profile.html', person=person)
     
     @app.route("/halloween")
     def halloweenpage(): 
